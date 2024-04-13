72a814b65926207ea224ea596373bd6a3b7e3180
Gabriel Abreu <gabrielabreu.development@gmail.com>
2024-04-13 02:50:33 -0300

    #006: More pdf tests

--- api/tests/Cramming.IntegrationTests/Pdf/NotecardsPdfServiceTests.cs
+++ api/tests/Cramming.IntegrationTests/Pdf/NotecardsPdfServiceTests.cs
@@ -21,5 +21,37 @@ namespace Cramming.IntegrationTests.Pdf
             var content = _service.Create(model);
             content.Should().NotBeNull();
         }
+
+        [Fact]
+        public void CreateWithTagsReturnsBinaryContent()
+        {
+            var model = new Topic("Topic");
+            model.AddTag("Tag 1", "#808080");
+            model.AddTag("Tag 2", string.Empty);
+            var content = _service.Create(model);
+            content.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void CreateWithOpenEndedQuestionReturnsBinaryContent()
+        {
+            var model = new Topic("Topic Name");
+            model.AddOpenEndedQuestion("Statement 1", "Answer 1");
+            model.AddOpenEndedQuestion("Statement 2", "Answer 2");
+            model.AddOpenEndedQuestion("Statement 3", "Answer 3");
+            var content = _service.Create(model);
+            content.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void CreateWithMultipleChoiceQuestionReturnsBinaryContent()
+        {
+            var model = new Topic("Topic Name");
+            model.AddMultipleChoiceQuestion("Statement 1").AddOption("Option 1", true);
+            model.AddMultipleChoiceQuestion("Statement 2").AddOption("Option 2.1", false);
+            model.AddMultipleChoiceQuestion("Statement 3");
+            var content = _service.Create(model);
+            content.Should().NotBeNull();
+        }
     }
 }
